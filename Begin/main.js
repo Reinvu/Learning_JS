4a44ce1c89d30f9497161d174d18364d960a9257
Reinvu <shapavel2000@gmail.com>
2026-08-25 17:32:31 +0500

    Начало изучения

---
+++ Begin/main.js
@@ -0,0 +1,26 @@
+// let num = 18;
+// let str = "str";
+// let un
+// let bool = true
+// console.log(typeof(num))
+// console.log(typeof(str))
+// console.log(typeof(un))
+// console.log(typeof(bool))
+
+
+// let hours = prompt("Сколько сейчас часов ?")
+// let minutes = prompt("Сколько сейчас минут ?")
+// let time1 = (24 - hours)
+// let time2 = (60 - minutes)
+// alert(`Осталось до следующего дня ${time1} часов ${time2} минут`)
+
+
+// let number = prompt("Введите число: ")
+// let result = number.slice(-1) + number.slice(0, -1)
+// alert(result)
+
+
+// let salary = 250
+// let sale = prompt("Введите общую сумму продаж: ")
+// let sumSalary = salary + sale * 0.10
+// alert(sumSalary)
